9469d07bfa1559404fde0eb41788de1c8a060fff
Yuvraj <yuvi16580@gmail.com>
2026-09-01 10:54:13 +0530

    This is simple calculator code

--- GIT-PythonCI/calculator.py
+++ GIT-PythonCI/calculator.py
@@ -4,7 +4,7 @@ def subtract(a, b):
 return a - b 
 def multiply(a, b): 
 return a * b 
-if __name__ == "__main__": 
+if __name__ == "__master__": 
 print("2 + 3 =", add(2, 3)) 
 print("10 - 4 =", subtract(10, 4)) 
 print("3 * 5 =", multiply(3, 5)) 
